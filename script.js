00815cb6d974cc778294a2a45e7d9428a0c0f893
MinhajSadik <MinhajoSadik@gmail.com>
2022-11-30 21:35:22 +0600

    [TRYING] to dynamic content with the help of DOM

---
+++ script.js
@@ -0,0 +1,60 @@
+fetch('./dua.json')
+    .then((res) => res.json())
+    .then((data) => appendData(data))
+    .catch((error) =>
+        console.error('error:', error.message)
+    );
+
+function appendData(data) {
+    for (let i = 0; i < data.length; i++) {
+        const dua = data[i]
+        // let div = document.createElement("div");
+        let tr = document.createElement('tr')
+        let td = document.createElement('td');
+        let p = document.createElement('p')
+        td.innerText = dua.title
+
+        // div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+        tr.appendChild(td)
+        tr.appendChild(p)
+    }
+}
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scroll() };
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function backTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+function search() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
